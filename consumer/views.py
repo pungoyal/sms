f79cb4d566d612b5d17d3c8dd67e445668ef0534
pungoyal <pungoyal@gmail.com>
2010-02-16 16:12:59 +0530

    saving

--- consumer/views.py
+++ consumer/views.py
@@ -5,7 +5,7 @@ def index(request):
     return HttpResponse("Hello, world. You're at the poll index.")
 
 def add(request, phone_number, message):
-    print "***************ENTERED*************"
-    
     t = Text(number=phone_number, text=message)
     t.save()
+    
+    return HttpResponse("text:%s number:%s"  % (message, phone_number))
