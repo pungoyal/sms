88752466a90b78188a6a6304ee6dca8a87cf89c3
pungoyal <pungoyal@gmail.com>
2010-02-17 18:30:03 +0530

    unicode encoded response

--- consumer/views.py
+++ consumer/views.py
@@ -10,4 +10,4 @@ def add(request, phone_number, message):
     t = u'\u2a06\u3306\u2c06\u4a06\u4406'
     print str(t)
 
-    return HttpResponse(message.encode("utf-16"))
+    return HttpResponse(message)
