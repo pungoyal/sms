b6ed6fe448ff7d8d74dbe24d138f5464d1702cd5
pungoyal <pungoyal@gmail.com>
2010-02-17 18:46:33 +0530

    reverting to original

--- consumer/views.py
+++ consumer/views.py
@@ -7,7 +7,6 @@ def index(request):
 def add(request, phone_number, message):
     t = Text(number=phone_number, text=message)
     t.save()
-    t = u'\u2a06\u3306\u2c06\u4a06\u4406'
     print str(t)
 
     return HttpResponse(message)
